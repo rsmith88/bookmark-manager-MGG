42dce06ef7a86db5fdb4e3491a1cb11b28378f4d
Raylene <smith.raylene@gmail.com>
2018-10-25 15:38:47 +0100

    Added in Update page (not refactored)

--- MyApp.rb
+++ MyApp.rb
@@ -29,6 +29,18 @@ class MyApp < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark_id = params[:id]
+    erb :edit_bookmark
+  end
+
+  patch '/bookmarks/:id' do
+    #p params
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("UPDATE bookmarks SET url = '#{params[:url]}', title = '#{params[:title]}' WHERE id = '#{params[:id]}'")
+    redirect('/bookmarks')
+  end
+
   run! if app_file == $0
 
 end
--- README.md
+++ README.md
@@ -11,6 +11,13 @@ As a user
 So that I can save a favourite website
 I would like to add a favourite website to bookmarks.
 
+As a user
+So I can delete a bookmark I no longer need
+I want to delete a bookmark in Bookmark Manager
+
+As a user
+So I can change a bookmark in Bookmark Manager
+I want to update a bookmark
 
 
 ![Alt text](./Bookmarks_app_model.png)
