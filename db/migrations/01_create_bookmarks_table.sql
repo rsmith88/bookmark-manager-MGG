19d163a5e53b3a075b70be2b81f6402c0b703db3
Raylene <smith.raylene@gmail.com>
2018-10-26 16:59:20 +0100

    Added comments table and amended truncate command in setup_test_block

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,3 +1,6 @@
 
 in migrations/01_create_bookmarks_table.sql
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+
+03 create_comments_table
+CREATE TABLE comments(id SERIAL PRIMARY KEY, text VARCHAR(240), bookmark_id INTEGER REFERENCES bookmarks (id));
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,5 +3,5 @@ require 'pg'
 def setup_test_database
   "Setting up test database..."
   connection = PG.connect(dbname: 'bookmark_manager_test')
-  connection.exec("TRUNCATE bookmarks;")
+  connection.exec("TRUNCATE comments, bookmarks;")
 end
